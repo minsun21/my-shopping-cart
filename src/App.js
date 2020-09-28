7e8fe6c6fb12d3126714b2e5cc1ae6ce12e4b17b
minsun21 <mspark0429@daum.net>
2020-09-28 22:57:27 +0900

    add redux products

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import data from './data.json'
-import Product from './components/Products'
-import Filter from './components/Filter'
-import Cart from './components/Cart'
+import data from './data.json';
+import Product from './components/Products';
+import Filter from './components/Filter';
+import Cart from './components/Cart';
+import store from './store';
+import { Provider } from 'react-redux';
 
 class App extends React.Component {
   constructor() {
@@ -65,31 +67,33 @@ class App extends React.Component {
   }
   render() {
     return (
-      <div className="grid-container">
-        <header>
-          <a href="/">React Shopping Cart</a>
-        </header>
-        <main>
-          <div className="content">
-            <div className="main">
-              <Filter count={this.state.products.length}
-                size={this.state.size}
-                sort={this.state.sort}
-                filterProducts={this.filterProducts}
-                sortProducts={this.sortProducts}
-              />
-              <Product products={this.state.products} addToCart={this.addToCart} />
+      <Provider store={store}>
+        <div className="grid-container">
+          <header>
+            <a href="/">React Shopping Cart</a>
+          </header>
+          <main>
+            <div className="content">
+              <div className="main">
+                <Filter count={this.state.products.length}
+                  size={this.state.size}
+                  sort={this.state.sort}
+                  filterProducts={this.filterProducts}
+                  sortProducts={this.sortProducts}
+                />
+                <Product products={this.state.products} addToCart={this.addToCart} />
+              </div>
+              <div className="sidebar">
+                <Cart createOrder={this.createOrder} cartItems={this.state.cartItems} removeFromCart={this.removeFromCart} />
+              </div>
             </div>
-            <div className="sidebar">
-              <Cart createOrder={this.createOrder} cartItems={this.state.cartItems} removeFromCart={this.removeFromCart} />
-            </div>
-          </div>
 
-        </main>
-        <footer>
-          All right is reserved.
+          </main>
+          <footer>
+            All right is reserved.
     </footer>
-      </div>
+        </div>
+      </Provider>
     );
   }
 }
--- src/components/Products.js
+++ src/components/Products.js
@@ -3,13 +3,19 @@ import formatCurrency from '../util';
 import Fade from 'react-reveal/Fade';
 import Modal from 'react-modal';
 import Zoom from 'react-reveal/Zoom'
+import { connect } from 'react-redux';
+import { fetchProducts } from '../actions/productAction';
 
-export default class Products extends Component {
+class Products extends Component {
     constructor(props) {
         super(props);
         this.state = {
             product: null,
         };
+    }
+
+    componentDidMount() {
+        this.props.fetchProducts();
     }
     openModal = (product) => {
         this.setState({ product });
@@ -22,24 +28,27 @@ export default class Products extends Component {
         return (
             <div>
                 <Fade bottom cascade>
-                    <ul className="products">
-                        {this.props.products.map(product => (
-                            <li key={product._id}>
-                                <div className="product">
-                                    <a href={"#" + product._id} onClick={() => this.openModal(product)}>
-                                        <img src={product.image} alt="product img" />
-                                        <p>
-                                            {product.title}
-                                        </p>
-                                    </a>
-                                    <div className="product-price">
-                                        <div>{formatCurrency(product.price)}</div>
-                                        <button onClick={() => this.props.addToCart(product)} className="button primary">Add To Cart</button>
-                                    </div>
-                                </div>
-                            </li>
-                        ))}
-                    </ul>
+                    {!this.props.products ? (<div>Loading...</div>) :
+                        (
+                            <ul className="products">
+                                {this.props.products.map(product => (
+                                    <li key={product._id}>
+                                        <div className="product">
+                                            <a href={"#" + product._id} onClick={() => this.openModal(product)}>
+                                                <img src={product.image} alt="product img" />
+                                                <p>
+                                                    {product.title}
+                                                </p>
+                                            </a>
+                                            <div className="product-price">
+                                                <div>{formatCurrency(product.price)}</div>
+                                                <button onClick={() => this.props.addToCart(product)} className="button primary">Add To Cart</button>
+                                            </div>
+                                        </div>
+                                    </li>
+                                ))}
+                            </ul>
+                        )}
                 </Fade>
                 {product && (
                     <Modal isOpen={true}>
@@ -78,3 +87,4 @@ export default class Products extends Component {
     }
 }
 
+export default connect((state) => ({ product: state.products.items }), { fetchProducts })(Products);
